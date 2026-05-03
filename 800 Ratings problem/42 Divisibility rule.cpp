2e163f3d3fbbda2f39f2c2b563c7c04be4388ed5
Harivansh Singh <mukulrajawat34@gmail.com>
2026-05-03 20:24:34 +0530

    File

---
+++ 800 Ratings problem/42 Divisibility rule.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        long long a, b;
+        cin>>a>>b;
+        
+        long long moves=0;
+       
+        if(a%b==0)
+        {
+            cout<<moves<<endl;
+        }
+        else
+        {
+            cout<<b-(a%b)<<endl;
+        }
+    }
+    return 0;
+  
+}

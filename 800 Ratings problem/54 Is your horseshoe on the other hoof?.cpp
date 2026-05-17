a64972ffbcbf7eda4dc277856647fe96f37ddf98
Harivansh Singh <mukulrajawat34@gmail.com>
2026-05-17 21:31:10 +0530

    New Problem

---
+++ 800 Ratings problem/54 Is your horseshoe on the other hoof?.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> s(4);
+    for(int i=0; i<4; i++)
+    {
+    cin>>s[i];
+    }
+    int count=0;
+    
+    sort(s.begin(), s.end());
+    
+    for(int i=0; i<3; i++)
+    {
+        if(s[i]==s[i+1])
+        {
+            count++;
+        }
+    }
+    
+  
+    cout<<count;
+    return 0;
+}

a82c646de8917ba24d423eee5b2b5563a7f66a4e
Harivansh Singh <mukulrajawat34@gmail.com>
2026-06-02 15:06:52 +0530

    New problem handled

---
+++ 800 Ratings problem/73, 148A Insomnia Cure.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int k, l, m, n, d;
+    cin>>k;
+    cin>>l;
+    cin>>m;
+    cin>>n;
+    cin>>d;
+    
+    int count=0;
+    
+    for(int i=1; i<=d; i++)
+    {
+        if(i%k==0)
+        {
+            count++;
+        }
+        else if(i%l==0)
+        {
+            count++;
+        }
+        else if(i%m==0)
+        {
+            count++;
+        }
+        else if(i%n==0)
+        {
+            count++;
+        }
+    }
+    
+    cout<<count<<endl;
+    
+    return 0;
+}

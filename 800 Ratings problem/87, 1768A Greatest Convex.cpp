c90d6980fd6d2583341d809ef6f45d9f80b16d20
Harivansh Singh <mukulrajawat34@gmail.com>
2026-06-15 11:45:37 +0530

    New problem added

---
+++ 800 Ratings problem/87, 1768A Greatest Convex.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        long long k;
+        cin>>k;
+        
+        cout<<k-1<<endl;
+    }
+    
+    return 0;
+}

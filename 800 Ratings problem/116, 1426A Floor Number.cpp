9e603008113edd86526e5563a27120fcdff5d4d8
Harivansh Singh <mukulrajawat34@gmail.com>
2026-06-26 12:50:02 +0530

    New problem added

---
+++ 800 Ratings problem/116, 1426A Floor Number.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n, x;
+        cin>>n>>x;
+        
+        int floor=0;
+        
+        if(n<=2)
+        {
+            cout<<1<<endl;
+        }
+        else
+        {
+            floor=(n-3)/x+2;
+            cout<<floor<<endl;
+        }
+    }
+    
+    return 0;
+}

1ed015e4babae7ff6a300b20ff01de9b8b9c3230
Harivansh Singh <mukulrajawat34@gmail.com>
2026-07-04 14:32:40 +0530

    New Problem 1296A added

---
+++ 800 Ratings problem/133, 1296A Array with odd sum.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        vector<int> v;
+        
+        int sum=0;
+        
+        for(int i=0; i<n; i++)
+        {
+            int a;
+            cin>>a;
+            
+            v.push_back(a);
+            sum+=v[i];
+        }
+        
+        int odd=0;
+        int even=0;
+        
+        if(sum%2!=0)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            for(int i=0; i<n; i++)
+           {
+            if(v[i]%2==0)  
+             {
+                even++;
+             }
+            else
+             {
+                odd++;
+             }
+           }
+           if(odd>0 && even>0)
+           {
+               cout<<"YES"<<endl;
+           }
+           else
+           {
+               cout<<"NO"<<endl;
+           }
+        }
+        
+
+    }
+    
+    return 0;
+}

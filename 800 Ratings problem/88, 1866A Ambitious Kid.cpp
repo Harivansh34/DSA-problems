6f8790bf61e03b6614c0a75db2735eb4af862397
Harivansh Singh <mukulrajawat34@gmail.com>
2026-07-16 21:00:24 +0530

    New problem 1866A added

---
+++ 800 Ratings problem/88, 1866A Ambitious Kid.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    vector<int> v;
+    
+    for(int i=0; i<n; i++)
+    {
+        int a;
+        cin>>a;
+        
+        v.push_back(a);
+    }
+    
+    for(int i=0; i<n; i++)
+    {
+        if(v[i]<0)
+        {
+            v[i]=-v[i];
+        }
+    }
+    sort(v.begin(), v.end());
+    
+    cout<<v[0]<<endl;
+   
+   return 0;
+}

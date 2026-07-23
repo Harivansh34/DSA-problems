02c0e33eecffbcc9e3534a4e1446e1561bf3f6a9
Harivansh Singh <mukulrajawat34@gmail.com>
2026-07-23 20:56:33 +0530

    New Problem added

---
+++ 800 Ratings problem/157, 935A Fafa and company.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int count=0;
+    
+    if(n==2)
+    {
+        cout<<1<<endl;
+    }
+    else
+    {
+        for(int i=1; i<n; i++)
+        {
+            if(n%i==0 || i==1)
+            {
+                count++;
+            }
+        }
+        
+        cout<<count<<endl;
+    }
+    
+    return 0;
+    
+}

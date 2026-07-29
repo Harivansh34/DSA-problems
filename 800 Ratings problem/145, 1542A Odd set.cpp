9c8a27ca4dc129cf15a6af94c08ef38abbe43997
Harivansh Singh <mukulrajawat34@gmail.com>
2026-07-29 22:34:24 +0530

    New Problem 1542A added

---
+++ 800 Ratings problem/145, 1542A Odd set.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        vector<int> v;
+        
+        for(int i=0; i<n*2; i++)
+        {
+            int a;
+            cin>>a;
+            
+            v.push_back(a);
+        }
+        
+        int odd=0;
+        int even=0;
+        
+        for(int i=0; i<n*2; i++)
+        {
+            if(v[i]%2==0)
+            {
+               even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+        
+       if(even==odd)
+       {
+           cout<<"YES"<<endl;
+       }
+       else
+       {
+           cout<<"NO"<<endl;
+       }
+        
+    }
+    
+    return 0;
+}

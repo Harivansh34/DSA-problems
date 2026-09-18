67a0126792798746fae9834c1a778d082681a95a
Harivansh Singh <mukulrajawat34@gmail.com>
2026-09-18 15:04:11 +0530

    New Problem 1829A added

---
+++ 800 Ratings problem/208, 1829A Love story.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+        {
+            string s;
+            cin>>s;
+
+            string r="codeforces";
+            int count=0;
+            
+            for(int i=0; i<10; i++)
+                {
+                    if(s[i]!=r[i])
+                    {
+                        count++;
+                    }
+                }
+
+            cout<<count<<endl;
+        }
+    return 0;
+}
